1e3ea93a93a9d37e1d9951806832df949f2150e4
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2025-06-02 19:40:14 +0100

    Commit

--- count_words.c
+++ count_words.c
@@ -1,21 +1,23 @@
 #include <stdio.h>
 
 int main() {
-    char str[200];
-    int i = 0, inWord = 0, wordCount = 0;
+    char str[100];
+    int i = 0, count = 0, inWord = 0;
 
-    fgets(str, sizeof(str), stdin);
+    fgets(str, sizeof(str), stdin); // Lecture sécurisée
 
     while (str[i] != '\0') {
-        if (str[i] != ' ' && str[i] != '\n' && !inWord) {
-            inWord = 1;
-            wordCount++;
-        } else if (str[i] == ' ' || str[i] == '\n') {
-            inWord = 0;
+        if (str[i] != ' ' && str[i] != '\t' && str[i] != '\n') {
+            if (!inWord) {
+                inWord = 1; // Début d'un mot
+                count++;
+            }
+        } else {
+            inWord = 0; // Fin d'un mot
         }
         i++;
     }
 
-    printf("Nombre de mots : %d\n", wordCount);
+    printf("Nombre de mots : %d\n", count);
     return 0;
 }
--- replace_char.c
+++ replace_char.c
@@ -1,22 +1,29 @@
 #include <stdio.h>
+#include <string.h>
+
+#define MAX_LENGTH 256
 
 int main() {
-    char str[100], oldChar, newChar;
-    int i = 0;
+    char chaine[MAX_LENGTH];
+    char car_a_remplacer, car_de_remplacement;
 
-    fgets(str, sizeof(str), stdin);
+    // Demander à l'utilisateur d'entrer la chaîne
+    fgets(chaine, MAX_LENGTH, stdin);
+    chaine[strcspn(chaine, "\n")] = 0; // Supprimer le caractère de fin de ligne
 
-    scanf(" %c", &oldChar);
+    // Demander à l'utilisateur d'entrer le caractère à remplacer et le caractère de remplacement
+    scanf(" %c", &car_a_remplacer); // Notez le espace avant %c pour consommer des caractères en excès
+    scanf(" %c", &car_de_remplacement);
 
-    scanf(" %c", &newChar);
-
-    while (str[i] != '\0') {
-        if (str[i] == oldChar) {
-            str[i] = newChar;
+    // Parcourir la chaîne et remplacer les caractères
+    for (int i = 0; chaine[i] != '\0'; i++) {
+        if (chaine[i] == car_a_remplacer) {
+            chaine[i] = car_de_remplacement;
         }
-        i++;
     }
 
-    printf("Résultat : %s", str);
+    // Afficher la chaîne modifiée
+    printf("Résultat : %s\n", chaine);
+
     return 0;
 }
