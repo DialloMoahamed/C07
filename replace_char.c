cfe19ba140a9e83bec3b5998d4f5c744478bf741
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2025-06-02 21:27:52 +0100

    Commit

--- replace_char.c
+++ replace_char.c
@@ -1,24 +1,28 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
-    char chaine[100];
-    char car_a_remplacer, car_de_remplacement;
-    int i;
+  char chaine[100], caractere_a_remplacer, caractere_de_remplacement;
 
-    // Lire la chaîne + les deux caractères sur une seule ligne
-    scanf("%s", chaine);
-    scanf(" %c",&car_a_remplacer);
-    scanf(" %c", &car_de_remplacement);
+  // Demander à l'utilisateur de saisir la chaîne
+  fgets(chaine, sizeof(chaine), stdin); // Utilisation de gets peut être dangereuse, utiliser fgets à la place en production
 
-    // Remplacement
-    for (i = 0; chaine[i] != '\0'; i++) {
-        if (chaine[i] == car_a_remplacer) {
-            chaine[i] = car_de_remplacement;
-        }
+  // Demander à l'utilisateur le caractère à remplacer
+  scanf(" %c", &caractere_a_remplacer); // Notez le espace avant %c pour éviter les problèmes de lecture des caractères spéciaux
+
+  // Demander à l'utilisateur le caractère de remplacement
+  scanf(" %c", &caractere_de_remplacement);
+
+  // Parcourir la chaîne et remplacer les caractères
+  int i;
+  for (i = 0; chaine[i] != '\0'; i++) {
+    if (chaine[i] == caractere_a_remplacer) {
+      chaine[i] = caractere_de_remplacement;
     }
+  }
 
-    // Affichage exactement conforme
-    printf("Résultat àprés remplacement : %s\n", chaine);
+  // Afficher la chaîne modifiée
+  printf("Résultat: %s\n", chaine);
 
-    return 0;
+  return 0;
 }
