2cab9611f96d42c7908edd85df463a5b2c5c97c3
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2025-06-02 20:07:29 +0100

    Commit

--- replace_char.c
+++ replace_char.c
@@ -4,25 +4,24 @@
 #define MAX_LENGTH 256
 
 int main() {
+    char input[MAX_LENGTH];
     char chaine[MAX_LENGTH];
     char car_a_remplacer, car_de_remplacement;
 
-    // Demander à l'utilisateur d'entrer la chaîne
-    fgets(chaine, MAX_LENGTH, stdin);
-    chaine[strcspn(chaine, "\n")] = 0; // Supprimer le caractère de fin de ligne
+    // Lire toute la ligne
+    fgets(input, MAX_LENGTH, stdin);
 
-    // Demander à l'utilisateur d'entrer le caractère à remplacer et le caractère de remplacement
-    scanf(" %c", &car_a_remplacer); // Notez le espace avant %c pour consommer des caractères en excès
-    scanf(" %c", &car_de_remplacement);
+    // Extraire la chaîne et les caractères avec sscanf
+    sscanf(input, "%s %c %c", chaine, &car_a_remplacer, &car_de_remplacement);
 
-    // Parcourir la chaîne et remplacer les caractères
+    // Remplacer les caractères dans la chaîne
     for (int i = 0; chaine[i] != '\0'; i++) {
         if (chaine[i] == car_a_remplacer) {
             chaine[i] = car_de_remplacement;
         }
     }
 
-    // Afficher la chaîne modifiée
+    // Afficher le résultat
     printf("Résultat : %s\n", chaine);
 
     return 0;
