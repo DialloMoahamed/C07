5d4f64807d9bb8bbf1fcca35636cf5b79a2261a1
DialloMoahamed <mohamedattahirdiallo@gmail.com>
2025-06-02 20:15:31 +0100

    Commit

--- replace_char.c
+++ replace_char.c
@@ -1,27 +1,21 @@
 #include <stdio.h>
-#include <string.h>
-
-#define MAX_LENGTH 256
 
 int main() {
-    char input[MAX_LENGTH];
-    char chaine[MAX_LENGTH];
+    char chaine[100];
     char car_a_remplacer, car_de_remplacement;
+    int i;
 
-    // Lire toute la ligne
-    fgets(input, MAX_LENGTH, stdin);
+    // Lire la chaîne + les deux caractères sur une seule ligne
+    scanf("%s %c %c", chaine, &car_a_remplacer, &car_de_remplacement);
 
-    // Extraire la chaîne et les caractères avec sscanf
-    sscanf(input, "%s %c %c", chaine, &car_a_remplacer, &car_de_remplacement);
-
-    // Remplacer les caractères dans la chaîne
-    for (int i = 0; chaine[i] != '\0'; i++) {
+    // Remplacement
+    for (i = 0; chaine[i] != '\0'; i++) {
         if (chaine[i] == car_a_remplacer) {
             chaine[i] = car_de_remplacement;
         }
     }
 
-    // Afficher le résultat
+    // Affichage conforme à l'exemple
     printf("Résultat : %s\n", chaine);
 
     return 0;
